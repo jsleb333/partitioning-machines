a667388d617e0f15f4e751ba45cc1c95494882e1
Jean-Samuel Leboeuf <jean-samuel.leboeuf@hotmail.com>
2020-04-23 09:42:31 -0400

    fix init

--- partitioning_machines/__init__.py
+++ partitioning_machines/__init__.py
@@ -1,3 +1,4 @@
-from .tree import *
-from .partitioning_function_upper_bound import *
-from .vcdim import *
+from partitioning_machines.tree import *
+from partitioning_machines.partitioning_function_upper_bound import *
+from partitioning_machines.vcdim import *
+from partitioning_machines.utils import *
