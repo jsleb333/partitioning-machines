251893fe634bafd7712d213d2653f090a7a51a57
Jean-Samuel Leboeuf <jean-samuel.leboeuf@hotmail.com>
2020-04-24 15:56:33 -0400

    add doc

--- partitioning_machines/tree.py
+++ partitioning_machines/tree.py
@@ -5,11 +5,22 @@ from sympy.functions.combinatorial.numbers import stirling
 
 
 class Tree:
+    """
+    This Tree class implements a binary tree object in a recursive manner, i.e. its 'left_subtree' and 'right_subtree' attributes are other Tree objects.
+
+    The class implements the '__eq__' operator to be able to compare other trees. It returns true if both trees are non-equivalent, i.e. it does not matter which subtree is the left and the right (they can be swapped).
+
+    The class handles the number of leaves of the whole tree in the property 'n_leaves', which computes it once the first time it is accessed and then stores it in the '_n_leaves' attributes (and for every subtrees of the tree as well). The number of leaves is computed via the method 'update_tree'.
+
+    The class also implements a hash so that Tree objects can be used as key in a dictionary. The chosen hashing function here is the sum of the hash value of the two subtrees and of the total number of leaves of the tree. The hash is computed once when the function 'update_tree' is called and is then stored in the '_hash_value' property.
+    """
     def __init__(self, left_subtree=None, right_subtree=None):
         """
         Args:
-            left_subtree ():
-            right_subtree ():
+            left_subtree (Tree object): Other Tree object acting as the left subtree. If None, it means the present tree is a leaf.*
+            right_subtree (Tree object): Other Tree object acting as the right subtree. If None, it means the present tree is a leaf.*
+
+        *To be a valid tree, both left_subtree and right_subtree must be Tree objects or both must be None.
         """
         if left_subtree is None and right_subtree is not None or left_subtree is not None and right_subtree is None:
             raise ValueError('Both subtrees must be either None or other valid trees.')
@@ -26,6 +37,11 @@ class Tree:
         return self._n_leaves
 
     def update_tree(self):
+        """
+        Updated the number of leaves and the hash value of the tree and all its subtrees. Is called the first time 'n_leaves' or 'hash_value' is accessed.
+
+        If at some point one of the subtrees is modified by the user, this method should be called to count the new number of leaves and the new hash value of the tree.
+        """
         if self.is_leaf():
             self._n_leaves = 1
             self._hash_value = 0
@@ -41,14 +57,23 @@ class Tree:
         return self._hash_value
 
     def is_leaf(self):
+        """
+        A leaf is a tree with no subtrees.
+        """
         return self.left_subtree is None and self.right_subtree is None
 
     def is_stump(self):
+        """
+        A stump is a tree with two leaves as subtrees.
+        """
         if self.is_leaf():
             return False
         return self.left_subtree.is_leaf() and self.right_subtree.is_leaf()
 
     def __eq__(self, other):
+        """
+        Check if both trees share the same structure, up to equivalencies.
+        """
         if other is None:
             return False
         if self.is_leaf() and other.is_leaf():
