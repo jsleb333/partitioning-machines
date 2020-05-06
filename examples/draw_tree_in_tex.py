a7467c2a2c7697fd5f8308cff77115340fa42995
Jean-Samuel Leboeuf <jean-samuel.leboeuf@hotmail.com>
2020-05-06 13:09:28 -0400

    example based on iris and sklearn

--- examples/draw_tree_in_tex.py
+++ examples/draw_tree_in_tex.py
@@ -1,39 +1,23 @@
 """
 In this script, we show a minimal working example on how to draw a tree in tex with python2latex.
 """
-from partitioning_machines import Tree, tree_to_tikz, draw_tree
+from partitioning_machines import Tree, tree_to_tikz, draw_tree, tree_from_sklearn_decision_tree
 import python2latex as p2l
 
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.datasets import load_iris
+import numpy as np
 
-leaf = Tree() # tree 1
-stump = Tree(leaf, leaf) # tree 2
-tree3 = Tree(stump, leaf)
-tree4 = Tree(stump, stump)
-tree5 = Tree(tree3, leaf)
-tree6 = Tree(tree4, leaf)
-tree7 = Tree(tree3, stump)
-tree8 = Tree(tree3, tree3)
-tree9 = Tree(tree4, stump)
-tree10 = Tree(tree4, tree3)
-tree11 = Tree(tree4, tree4)
+X, y = load_iris(return_X_y=True)
+sklearn_tree = DecisionTreeClassifier()
+sklearn_tree = sklearn_tree.fit(X, y)
 
-trees = [
-    leaf,
-    stump,
-    tree3,
-    tree4,
-    tree5,
-    tree6,
-    tree7,
-    tree8,
-    tree9,
-    tree10,
-    tree11
-]
+tree = tree_from_sklearn_decision_tree(sklearn_tree)
 
-tikzpicture_object = tree_to_tikz(trees[8])
+
+tikzpicture_object = tree_to_tikz(tree)
 
 print(tikzpicture_object.build()) # Converts object to string usable in tex file
 
 # Draw tree in LaTeX if pdflatex is available
-draw_tree(trees[8])
+draw_tree(tree)
