f8a2354b6fca977d918b1d56644438e9311486da
Jean-Samuel Leboeuf <jean-samuel.leboeuf@hotmail.com>
2020-05-06 12:13:48 -0400

    simplified code

--- partitioning_machines/utils/draw_tree.py
+++ partitioning_machines/utils/draw_tree.py
@@ -4,144 +4,37 @@ try:
 except ImportError:
     raise ImportError("The drawing of trees rely on the package python2latex. Please install it with 'pip install python2latex'.")
 
-class VectorTree:
-    def __init__(self, recursive_tree):
-        self.recursive_tree = recursive_tree
-
-        self.n_nodes = 2*recursive_tree.n_leaves-1
-        self.left_subtrees = [0]*self.n_nodes
-        self.right_subtrees = [0]*self.n_nodes
-        self.layers = [0]*self.n_nodes
-        self.positions = [0]*self.n_nodes
-
-        self._vectorize_tree()
-        self._deoverlap_tree()
-
-    def _vectorize_tree(self):
-        layer = 0
-        node_id = 0
-        subtrees_in_layer = [(node_id, self.recursive_tree)]
-        child_node_id = 1
-        for layer in range(self.recursive_tree.depth+1):
-            subtrees_in_next_layer = []
-            for node_id, subtree in subtrees_in_layer:
-                self.layers[node_id] = layer
-
-                if subtree.is_leaf():
-                    self.left_subtrees[node_id] = -1
-                    self.right_subtrees[node_id] = -1
-                else:
-                    self.left_subtrees[node_id] = child_node_id
-                    self.positions[child_node_id] = self.positions[node_id] - 1
-                    subtrees_in_next_layer.append((child_node_id, subtree.left_subtree))
-                    child_node_id += 1
-
-                    self.right_subtrees[node_id] = child_node_id
-                    self.positions[child_node_id] = self.positions[node_id] + 1
-                    subtrees_in_next_layer.append((child_node_id, subtree.right_subtree))
-                    child_node_id += 1
-
-            subtrees_in_layer = subtrees_in_next_layer
-
-    def node_is_leaf(self, node):
-        return self.left_subtrees[node] == -1 and self.right_subtrees[node] == -1
-
-    def _deoverlap_tree(self, node=0):
-        if self.node_is_leaf(node):
-            return
-        else:
-            left_node = self.left_subtrees[node]
-            right_node = self.right_subtrees[node]
-            self._deoverlap_tree(left_node)
-            self._deoverlap_tree(right_node)
-            overlap = self._find_largest_overlap(left_node, right_node)
-            if overlap >= -1:
-                self._shift_tree(left_node, -overlap/2 - 1)
-                self._shift_tree(right_node, overlap/2 + 1)
-
-    def _find_largest_overlap(self, left_node, right_node):
-        rightest_positions = self._find_rightest_positions_by_layer(left_node)
-        leftest_positions = self._find_leftest_positions_by_layer(right_node)
-        overlaps = [r - l for l, r in zip(leftest_positions, rightest_positions)]
-        return max(overlaps)
-
-    def _find_rightest_positions_by_layer(self, node):
-        rightest_positions_by_layer = []
-        nodes_in_layer = [node]
-        while nodes_in_layer:
-            nodes_in_next_layer = []
-            max_pos = self.positions[nodes_in_layer[0]]
-            for node in nodes_in_layer:
-                if self.positions[node] > max_pos:
-                    max_pos = self.positions[node]
-
-                if not self.node_is_leaf(node):
-                    nodes_in_next_layer.append(self.left_subtrees[node])
-                    nodes_in_next_layer.append(self.right_subtrees[node])
-            rightest_positions_by_layer.append(max_pos)
-            nodes_in_layer = nodes_in_next_layer
-
-        return rightest_positions_by_layer
-
-    def _find_leftest_positions_by_layer(self, node):
-        leftest_positions_by_layer = []
-        nodes_in_layer = [node]
-        while nodes_in_layer:
-            nodes_in_next_layer = []
-            min_pos = self.positions[nodes_in_layer[0]]
-            for node in nodes_in_layer:
-                if self.positions[node] < min_pos:
-                    min_pos = self.positions[node]
-
-                if not self.node_is_leaf(node):
-                    nodes_in_next_layer.append(self.left_subtrees[node])
-                    nodes_in_next_layer.append(self.right_subtrees[node])
-            leftest_positions_by_layer.append(min_pos)
-            nodes_in_layer = nodes_in_next_layer
-
-        return leftest_positions_by_layer
-
-    def _shift_tree(self, node, shift):
-        self.positions[node] += shift
-        if self.node_is_leaf(node):
-            return
-        else:
-            self._shift_tree(self.left_subtrees[node], shift)
-            self._shift_tree(self.right_subtrees[node], shift)
-
 
 def tree_to_tikz(tree, min_node_distance=1.3, layer_distance=1.6, node_size=.6):
-    tree = VectorTree(tree)
 
     pic = p2l.TexEnvironment('tikzpicture')
     pic.options += f"""leaf/.style={{draw, diamond, minimum width={node_size}cm, minimum height={2*node_size}cm, inner sep=0pt}}""",
     pic.options += f"""internal/.style={{draw, circle, minimum width={node_size}cm, inner sep=0pt}}""",
 
-    for node in range(tree.n_nodes):
-        style = 'leaf' if tree.node_is_leaf(node) else 'internal'
-        pic += f'\\node[{style}](node{node}) at ({min_node_distance*tree.positions[node]/2:.3f}, {-layer_distance*tree.layers[node]:.3f}) {{}};'
+    for subtree in tree:
+        node = subtree.current_node
+        style = 'leaf' if subtree.is_leaf() else 'internal'
+        pic += f'\\node[{style}](node{node}) at ({min_node_distance*subtree.position/2:.3f}, {-layer_distance*subtree.layer:.3f}) {{}};'
 
-    for node in range(tree.n_nodes):
-        if not tree.node_is_leaf(node):
-            left_node = tree.left_subtrees[node]
-            right_node = tree.right_subtrees[node]
+    for node, subtree in enumerate(tree):
+        if not subtree.is_leaf():
 
-            if tree.node_is_leaf(left_node):
-                pic += f'\\draw (node{node}) -- (node{left_node}.north);'
+            if subtree.left_subtree.is_leaf():
+                pic += f'\\draw (node{node}) -- (node{subtree.left_child}.north);'
             else:
-                pic += f'\\draw (node{node}) -- (node{left_node});'
+                pic += f'\\draw (node{node}) -- (node{subtree.left_child});'
 
-            if tree.node_is_leaf(right_node):
-                pic += f'\\draw (node{node}) -- (node{right_node}.north);'
+            if subtree.right_subtree.is_leaf():
+                pic += f'\\draw (node{node}) -- (node{subtree.right_child}.north);'
             else:
-                pic += f'\\draw (node{node}) -- (node{right_node});'
+                pic += f'\\draw (node{node}) -- (node{subtree.right_child});'
 
     return pic
 
-def draw_tree(tree):
+def draw_tree(tree, show_pdf=True):
     doc = p2l.Document(str(tree).replace(' ', '_'), options=('varwidth',), doc_type='standalone', border='1cm')
     doc.add_package('tikz')
     del doc.packages['geometry']
     doc.add_to_preamble('\\usetikzlibrary{shapes}')
     doc += tree_to_tikz(tree)
-    doc.build()
+    doc.build(show_pdf=show_pdf)
--- tests/test_utils/test_draw_tree.py
+++ tests/test_utils/test_draw_tree.py
@@ -1,129 +1,33 @@
 from partitioning_machines import Tree
-from partitioning_machines.utils.draw_tree import VectorTree, draw_tree
+from partitioning_machines.utils.draw_tree import draw_tree, tree_to_tikz
 from pytest import fixture
+
+import os, sys
+
 
 @fixture
 def trees():
-    leaf = Tree() # tree 1
-    stump = Tree(leaf, leaf) # tree 2
-    tree3 = Tree(stump, leaf)
-    tree4 = Tree(stump, stump)
-    tree5 = Tree(tree3, leaf)
-    tree6 = Tree(tree4, leaf)
-    tree7 = Tree(tree3, stump)
-    tree8 = Tree(tree3, tree3)
-    tree9 = Tree(tree4, stump)
-    tree10 = Tree(tree4, tree3)
-    tree11 = Tree(tree4, tree4)
-
-    return [leaf,
-            stump,
-            tree3,
-            tree4,
-            tree5,
-            tree6,
-            tree7,
-            tree8,
-            tree9,
-            tree10,
-            tree11]
-
-@fixture
-def overlapping_vectorized_trees(trees):
-    actual_init = VectorTree.__init__
-    VectorTree.__init__ = lambda *args: None
-    vectorized_trees = []
-    for tree in trees:
-        v_tree = VectorTree()
-        v_tree.__init__ = actual_init
-        v_tree.left_subtrees = [0]*(2*tree.n_leaves-1)
-        v_tree.right_subtrees = [0]*(2*tree.n_leaves-1)
-        v_tree.layers = [0]*(2*tree.n_leaves-1)
-        v_tree.positions = [0]*(2*tree.n_leaves-1)
-        v_tree.recursive_tree = tree
-        v_tree._vectorize_tree()
-        vectorized_trees.append(v_tree)
-    VectorTree.__init__ = actual_init
-
-    return vectorized_trees
+    trees = [Tree()]
+    trees.append(Tree(trees[0], trees[0]))
+    trees.append(Tree(trees[1], trees[0]))
+    trees.append(Tree(trees[1], trees[1]))
+    trees.append(Tree(trees[2], trees[0]))
+    trees.append(Tree(trees[2], trees[1]))
+    trees.append(Tree(trees[3], trees[0]))
+    trees.append(Tree(trees[2], trees[2]))
+    trees.append(Tree(trees[3], trees[1]))
+    trees.append(Tree(trees[3], trees[2]))
+    trees.append(Tree(trees[3], trees[3]))
+    return trees
 
 
-class TestVectorTree:
-    def test_vectorize_tree(self, overlapping_vectorized_trees):
-        vectorized_tree = overlapping_vectorized_trees[2]
+def test_tree_to_tikz(trees):
+    pic = tree_to_tikz(trees[9])
+    print(pic.build())
 
-        assert vectorized_tree.left_subtrees == [1,3,-1,-1,-1]
-        assert vectorized_tree.right_subtrees == [2,4,-1,-1,-1]
-        assert vectorized_tree.layers == [0,1,1,2,2]
-        assert vectorized_tree.positions == [0,-1,1,-2,0]
-
-        vectorized_tree = overlapping_vectorized_trees[10]
-
-        assert vectorized_tree.left_subtrees == [1,3,5,7,9,11,13] + [-1]*8
-        assert vectorized_tree.right_subtrees == [2,4,6,8,10,12,14] + [-1]*8
-        assert vectorized_tree.layers == [0,1,1,2,2,2,2] + [3]*8
-        assert vectorized_tree.positions == [0,-1,1,-2,0,0,2,-3,-1,-1,1,-1,1,1,3]
-
-    def test_node_is_leaf(self, overlapping_vectorized_trees):
-        vectorized_tree = overlapping_vectorized_trees[2]
-        assert vectorized_tree.node_is_leaf(2)
-        assert vectorized_tree.node_is_leaf(3)
-        assert vectorized_tree.node_is_leaf(4)
-
-    def test_find_rightest_positions_by_layer(self, overlapping_vectorized_trees):
-        vectorized_tree = overlapping_vectorized_trees[3]
-        assert vectorized_tree._find_rightest_positions_by_layer(0) == [0, 1, 2]
-        assert vectorized_tree._find_rightest_positions_by_layer(1) == [-1, 0]
-
-        vectorized_tree = overlapping_vectorized_trees[10]
-        assert vectorized_tree._find_rightest_positions_by_layer(0) == [0, 1, 2, 3]
-        assert vectorized_tree._find_rightest_positions_by_layer(1) == [-1, 0, 1]
-        assert vectorized_tree._find_rightest_positions_by_layer(2) == [1, 2, 3]
-        assert vectorized_tree._find_rightest_positions_by_layer(3) == [-2, -1]
-        assert vectorized_tree._find_rightest_positions_by_layer(4) == [0, 1]
-
-    def test_find_leftest_positions_by_layer(self, overlapping_vectorized_trees):
-        vectorized_tree = overlapping_vectorized_trees[3]
-        assert vectorized_tree._find_leftest_positions_by_layer(0) == [0, -1, -2]
-        assert vectorized_tree._find_leftest_positions_by_layer(2) == [1, 0]
-
-        vectorized_tree = overlapping_vectorized_trees[10]
-        assert vectorized_tree._find_leftest_positions_by_layer(0) == [0, -1, -2, -3]
-        assert vectorized_tree._find_leftest_positions_by_layer(1) == [-1, -2, -3]
-        assert vectorized_tree._find_leftest_positions_by_layer(2) == [1, 0, -1]
-        assert vectorized_tree._find_leftest_positions_by_layer(3) == [-2, -3]
-        assert vectorized_tree._find_leftest_positions_by_layer(4) == [0, -1]
-
-    def test_find_largest_overlap(self, overlapping_vectorized_trees):
-        vectorized_tree = overlapping_vectorized_trees[3]
-        assert vectorized_tree._find_largest_overlap(1, 2) == 0
-        assert vectorized_tree._find_largest_overlap(3, 4) == -2
-
-        vectorized_tree = overlapping_vectorized_trees[10]
-        assert vectorized_tree._find_largest_overlap(1, 2) == 2
-        assert vectorized_tree._find_largest_overlap(3, 4) == 0
-        assert vectorized_tree._find_largest_overlap(5, 6) == 0
-
-
-    def test_shift_tree(self, overlapping_vectorized_trees):
-        vectorized_tree = overlapping_vectorized_trees[3]
-        vectorized_tree._shift_tree(1, -1)
-        assert vectorized_tree.positions == [0,-2,1,-3,-1,0,2]
-
-        vectorized_tree = overlapping_vectorized_trees[10]
-        vectorized_tree._shift_tree(1, -2)
-        vectorized_tree._shift_tree(2, 2)
-        assert vectorized_tree.positions == [0,-3,3,-4,-2,2,4,-5,-3,-3,-1,1,3,3,5]
-
-    def test_deoverlap_tree(self, overlapping_vectorized_trees):
-        vectorized_tree = overlapping_vectorized_trees[3]
-        vectorized_tree._deoverlap_tree()
-        assert vectorized_tree.positions == [0,-2,2,-3,-1,1,3]
-
-        vectorized_tree = overlapping_vectorized_trees[7]
-        vectorized_tree._deoverlap_tree()
-        assert vectorized_tree.positions == [0,-2,2,-3,-1,1,3,-4,-2,0,2]
-
-        vectorized_tree = overlapping_vectorized_trees[10]
-        vectorized_tree._deoverlap_tree()
-        assert vectorized_tree.positions == [0,-4,4,-6,-2,2,6,-7,-5,-3,-1,1,3,5,7]
+def test_draw_tree(trees):
+    draw_tree(trees[9], show_pdf=False)
+    os.remove('./Tree_of_depth_3.log')
+    os.remove('./Tree_of_depth_3.tex')
+    os.remove('./Tree_of_depth_3.aux')
+    os.remove('./Tree_of_depth_3.pdf')
