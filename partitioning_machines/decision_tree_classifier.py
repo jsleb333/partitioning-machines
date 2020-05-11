318b93c027d66eb64d4eb539937203f22e9605c4
Jean-Samuel Leboeuf <jean-samuel.leboeuf@hotmail.com>
2020-05-11 09:03:09 -0400

    finishing skeleton

--- partitioning_machines/decision_tree_classifier.py
+++ partitioning_machines/decision_tree_classifier.py
@@ -8,19 +8,15 @@ class Tree(_Tree):
     def __init__(self,
                  impurity_score,
                  n_examples_by_label,
-                 *args,
                  rule_threshold=None,
                  rule_feature=None,
-                 **kwargs):
+                 left_subtree=left_subtree,
+                 right_subtree=right_subtree,
+                 parent=parent):
         super().__init__(left_subtree=left_subtree,
                          right_subtree=right_subtree,
-                         parent=parent,
-                         height=height,
-                         depth=depth,
-                         n_leaves=n_leaves,
-                         n_nodes=n_nodes,
-                         hash_value=hash_value,
-                         position=position)
+                         parent=parent)
+
         self.impurity_score = impurity_score
         self.n_examples_by_label = n_examples_by_label
         self.label = np.eye(n_examples_by_label.shape[0], k=np.argmax(self.n_examples_by_label))
@@ -58,7 +54,11 @@ class DecisionTreeClassifier:
         if self.max_n_leaves is None:
             self.max_n_leaves = n_examples
         
-        possible_splits = [Split(self.tree, X, encoded_y, X_idx_sorted)] # List of splits that can be produced.
+        n_examples_by_label = np.sum(encoded_y, axis=0)
+        self.tree = Tree(self.impurity_criterion(n_examples_by_label/n_examples),
+                         n_examples_by_label)
+        
+        possible_splits = [Splitter(self.tree, X, encoded_y, X_idx_sorted)] # List of splits that can be produced.
         
         while possible_splits and self.tree.n_leaves < self.max_n_leaves:
             best_split = possible_splits[0]
@@ -74,15 +74,9 @@ class DecisionTreeClassifier:
             
             if self.tree.n_leaves < self.max_n_leaves and self.tree.depth < self.max_depth:
                 if best_split.split_makes_gain():
-                    possible_splits.append(best_split.split_left_leaf())
-                    possible_splits.append(best_split.split_right_leaf())
+                    possible_splits.extend(best_split.leaves_splitters())
             
             possible_splits.remove(best_split)
-
-    def _init_tree(self, n_examples, encoded_y):
-        n_examples_by_label = np.sum(encoded_y, axis=0)
-        self.tree.n_examples_by_label = n_examples_by_label
-        self.tree.impurity_score = self.impurity_criterion(n_examples_by_label/n_examples)
 
     def predict(self, X):
         return self.label_encoder.decode_labels(np.array([self.tree.predict(x) for x in X]))
@@ -92,7 +86,7 @@ class DecisionTreeClassifier:
     
 
 
-class Split:
+class Splitter:
     def __init__(self, leaf, X, y, X_idx_sorted, impurity_criterion, optimization_mode):
         self.leaf = leaf
         self.X = X
@@ -124,17 +118,8 @@ class Split:
             n_examples_by_label_left += transfered_labels
             n_examples_by_label_right -= transfered_labels
             tmp_feature, tmp_impurity_score = self.argext(self._split_impurity_criterion(n_examples_by_label_left, n_examples_by_label_right, n_examples_left, n_examples_right))
-            if self.optimization_mode == 'min':
-                if tmp_impurity_score < self.impurity_score:
-                    self.impurity_score = tmp_impurity_score
-                    self.rule_feature = tmp_feature
-                    self.rule_threshold_idx = x_idx[self.rule_feature]
-                    self.n_examples_left = n_examples_left
-                    self.n_examples_right = n_examples_right
-                    self.n_examples_by_label_left = n_examples_by_label_left[:,self.rule_feature].copy()
-                    self.n_examples_by_label_right = n_examples_by_label_right[:,self.rule_feature].copy()
-            elif self.optimization_mode == 'max':
-                if tmp_impurity_score > self.impurity_score:
+            if (self.optimization_mode == 'min' and tmp_impurity_score < self.impurity_score) \
+                or (self.optimization_mode == 'max' and tmp_impurity_score > self.impurity_score):
                     self.impurity_score = tmp_impurity_score
                     self.rule_feature = tmp_feature
                     self.rule_threshold_idx = x_idx[self.rule_feature]
@@ -144,7 +129,44 @@ class Split:
                     self.n_examples_by_label_right = n_examples_by_label_right[:,self.rule_feature].copy()
         
         self.rule_threshold = (self.X[self.best_threshold_idx, self.rule_feature] + self.X[self.best_threshold_idx+1, self.rule_feature])/2
+
+    def argext(self, arr):
+        if self.optimization_mode == 'min':
+            extremum = np.argmin
+        elif self.optimization_mode == 'max':
+            extremum = np.argmax
+        extremum_idx = extremum(arr)
+        return extremum_idx, arr(extremum_idx)
+
+    def _split_impurity_criterion(self, n_examples_by_label_left, n_examples_by_label_right, n_examples_left, n_examples_right):
+        return (self._weighted_impurity_criterion(n_examples_by_label_left, n_examples_left) + 
+                self._weighted_impurity_criterion(n_examples_by_label_left, n_examples_left)) / \
+                (n_examples_left + n_examples_right)
     
+    def _weighted_impurity_criterion(self, n_examples_by_label, n_examples):
+        return self.impurity_criterion(n_examples_by_label/n_examples) * n_examples
+    
+    def split_makes_gain(self):
+        if self.optimization_mode == 'min':
+            return self.impurity_score < self.leaf.impurity_score
+        elif self.optimization_mode == 'max':
+            return self.impurity_score > self.leaf.impurity_score
+            
+    def apply_split(self):
+        impurity_left = self.impurity_criterion(self.n_examples_by_label_left/self.n_examples_left)
+        left_leaf = Tree(impurity_left, self.n_examples_by_label_left)
+        impurity_right = self.impurity_criterion(self.n_examples_by_label_right/self.n_examples_right)
+        right_leaf = Tree(impurity_right, self.n_examples_by_label_right)
+        self.leaf.left_subtree = left_leaf
+        self.leaf.right_subtree = right_leaf
+        self.leaf.update_tree()
+    
+    def leaves_splitters(self):
+        self._compute_split_X_idx_sorted()
+        left_splitter = type(self)(self.leaf.left_subtree, self.X, self.y, self.X_idx_sorted_left, self.impurity_criterion, self.optimization_mode)
+        right_splitter = type(self)(self.leaf.right_subtree, self.X, self.y, self.X_idx_sorted_left, self.impurity_criterion, self.optimization_mode)
+        return [left_splitter, right_splitter]
+        
     def _compute_split_X_idx_sorted(self):
         self.X_idx_sorted_left = np.zeros((self.n_examples_left, self.n_features))
         self.X_idx_sorted_right = np.zeros((self.n_examples_right, self.n_features))
@@ -160,42 +182,6 @@ class Split:
                     self.X_idx_sorted_right[right_x_pos, feat] = idx
                     right_x_pos += 1
     
-    def _weighted_impurity_criterion(self, n_examples_by_label, n_examples):
-        return self.impurity_criterion(n_examples_by_label/n_examples) * n_examples
-
-    def _split_impurity_criterion(self, n_examples_by_label_left, n_examples_by_label_right, n_examples_left, n_examples_right):
-        return (self._weighted_impurity_criterion(n_examples_by_label_left, n_examples_left) + 
-                self._weighted_impurity_criterion(n_examples_by_label_left, n_examples_left)) / \
-                (n_examples_left + n_examples_right)
-
-    def argext(self, arr):
-        if self.optimization_mode == 'min':
-            extremum = np.argmin
-        elif self.optimization_mode == 'max':
-            extremum = np.argmax
-        
-        extremum_idx = extremum(arr)
-        
-        return extremum_idx, arr(extremum_idx)
-    
-    def split_makes_gain(self):
-        if self.optimization_mode == 'min':
-            return self.impurity_score < self.leaf.impurity_score
-        elif self.optimization_mode == 'max':
-            return self.impurity_score > self.leaf.impurity_score
-            
-    @property
-    def stump(self):
-        left_leaf = Tree()
-        left_leaf.n_examples_by_label = self.n_examples_by_label_left
-        return 
-    
-    def split_left_leaf(self):
-        pass
-
-    def split_right_leaf(self):
-        pass
-
 
 def gini_impurity_criterion(frac_examples_by_label):
     return np.sum(frac_examples_by_label * (1 - frac_examples_by_label))
