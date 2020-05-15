2f3dbf15843251eb198244af966c4fde2d15beed
Jean-Samuel Leboeuf <jean-samuel.leboeuf@hotmail.com>
2020-05-15 14:16:03 -0400

    importing other attr from sklearn trees

--- partitioning_machines/utils/convert_tree.py
+++ partitioning_machines/utils/convert_tree.py
@@ -1,5 +1,5 @@
-from partitioning_machines.tree import Tree
-from partitioning_machines.decision_tree_classifier import DecisionTreeClassifier
+from partitioning_machines.decision_tree_classifier import DecisionTreeClassifier, _DecisionTree
+from partitioning_machines.decision_tree_classifier import _DecisionTree as Tree
 
 
 def tree_from_sklearn_decision_tree(sklearn_tree):
@@ -15,15 +15,18 @@ def tree_from_sklearn_decision_tree(sklearn_tree):
 
 def _build_tree_from_sklearn_tree(sklearn_tree, current_node=0):
     children_left, children_right = sklearn_tree.children_left, sklearn_tree.children_right
+    impurity_score = sklearn_tree.impurity[current_node]
+    n_examples_by_label = sklearn_tree.value[current_node].reshape(-1)
+    
     if children_left[current_node] == -1 and children_right[current_node] == -1:
-        leaf = Tree()
-        leaf.label = sklearn_tree.value
-        return leaf
+        subtree = Tree(impurity_score, n_examples_by_label)
     else:
         subtree = Tree(
-            _build_tree_from_sklearn_tree(sklearn_tree, children_left[current_node]),
-            _build_tree_from_sklearn_tree(sklearn_tree, children_right[current_node])
+            impurity_score,
+            n_examples_by_label,
+            rule_threshold=sklearn_tree.threshold[current_node],
+            rule_feature=sklearn_tree.feature[current_node],
+            left_subtree=_build_tree_from_sklearn_tree(sklearn_tree, children_left[current_node]),
+            right_subtree=_build_tree_from_sklearn_tree(sklearn_tree, children_right[current_node])
         )
-        subtree.rule_feature = sklearn_tree.feature
-        subtree.rule_threshold = sklearn_tree.threshold
-        return subtree
+    return subtree
