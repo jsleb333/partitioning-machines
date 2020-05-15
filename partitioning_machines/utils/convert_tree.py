769a072156088d1be258208d5c9f535f80da3260
Jean-Samuel Leboeuf <jean-samuel.leboeuf@hotmail.com>
2020-05-15 12:30:39 -0400

    converting from sklearn now copies label and rule too

--- partitioning_machines/utils/convert_tree.py
+++ partitioning_machines/utils/convert_tree.py
@@ -13,9 +13,14 @@ def tree_from_sklearn_decision_tree(sklearn_tree):
 def _build_tree_from_sklearn_tree(sklearn_tree, current_node=0):
     children_left, children_right = sklearn_tree.children_left, sklearn_tree.children_right
     if children_left[current_node] == -1 and children_right[current_node] == -1:
-        return Tree()
+        leaf = Tree()
+        leaf.label = sklearn_tree.value
+        return leaf
     else:
-        return Tree(
+        subtree = Tree(
             _build_tree_from_sklearn_tree(sklearn_tree, children_left[current_node]),
             _build_tree_from_sklearn_tree(sklearn_tree, children_right[current_node])
         )
+        subtree.rule_feature = sklearn_tree.feature
+        subtree.rule_threshold = sklearn_tree.threshold
+        return subtree
--- tests/test_utils/test_convert_tree.py
+++ tests/test_utils/test_convert_tree.py
@@ -5,21 +5,6 @@ import numpy as np
 from partitioning_machines import Tree, tree_from_sklearn_decision_tree
 
 
-def test_tree_from_sklearn_decision_tree_with_mock():
-    class MockTree:
-        pass
-    tree_ = MockTree()
-    tree_.children_left = np.array([1,3,-1,-1,-1])
-    tree_.children_right = np.array([2,4,-1,-1,-1])
-    mock_tree = MockTree()
-    mock_tree.tree_ = tree_
-    
-    tree_from_sklearn = tree_from_sklearn_decision_tree(mock_tree)
-    
-    leaf = Tree()
-    stump = Tree(leaf, leaf)
-    assert tree_from_sklearn == Tree(stump, leaf)
-
 def test_tree_from_sklearn_decision_tree_with_actual_tree():
     X, y = load_iris(return_X_y=True)
     sklearn_tree = DecisionTreeClassifier()
