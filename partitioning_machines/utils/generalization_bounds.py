2d57b3604838d87dd29b61e984983eb764ddc7f0
Jean-Samuel Leboeuf <jean-samuel.leboeuf@hotmail.com>
2020-05-15 16:10:58 -0400

    generalization bound added

--- partitioning_machines/utils/generalization_bounds.py
+++ partitioning_machines/utils/generalization_bounds.py
@@ -1,5 +1,6 @@
 import numpy as np
 from copy import copy
+from partitioning_machines import growth_function_upper_bound
 
 
 def shawe_taylor_bound(n_examples, n_errors, growth_function, hypothesis_class_index, delta=.05):
@@ -8,11 +9,21 @@ def shawe_taylor_bound(n_examples, n_errors, growth_function, hypothesis_class_i
     """
     p_d = 6/(np.pi**2*(hypothesis_class_index + 1))
     q_k = 6/(np.pi**2*(n_errors + 1))
-    epsilon = 2 * n_errors + 4 * np.log(growth_function(2 * n_examples) + np.log(4/(p_d*q_k*delta)))
+    epsilon = 2*n_errors + 4*np.log(float(growth_function(2*n_examples))) + np.log(4/(p_d*q_k*delta))
     return epsilon / n_examples
 
-def shawe_taylor_bound_pruning_objective(subtree):
-    copy_of_subtree = copy(subtree)
-    copy_of_subtree.left_subtree = None
-    copy_of_subtree.right_subtree = None
-    copy_of_subtree.tree_root.update_tree()
+def shawe_taylor_bound_pruning_objective_factory(n_features, table={}):
+    def shawe_taylor_bound_pruning_objective(subtree):
+        copy_of_subtree = copy(subtree)
+        copy_of_subtree.left_subtree = None
+        copy_of_subtree.right_subtree = None
+        copy_of_subtree.tree_root.update_tree()
+        
+        n_classes = copy_of_subtree.n_examples_by_label.shape[0]
+        growth_function = growth_function_upper_bound(copy_of_subtree, n_features, n_classes, table)
+        n_examples = copy_of_subtree.tree_root.n_examples
+        n_errors = copy_of_subtree.tree_root.n_errors
+        hypothesis_class_index = copy_of_subtree.tree_root.hash_value
+        
+        return shawe_taylor_bound(n_examples, n_errors, growth_function, hypothesis_class_index)
+    return shawe_taylor_bound_pruning_objective
