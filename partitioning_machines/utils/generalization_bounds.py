df185999485a9bd45d66f804629bc31a07a7832f
Jean-Samuel Leboeuf <jean-samuel.leboeuf@hotmail.com>
2020-05-21 09:28:49 -0400

    more customizable bounds

--- partitioning_machines/utils/generalization_bounds.py
+++ partitioning_machines/utils/generalization_bounds.py
@@ -1,19 +1,37 @@
 import numpy as np
 from copy import copy
+from scipy.special import zeta
+
 from partitioning_machines import growth_function_upper_bound
 
 
-def shawe_taylor_bound(n_examples, n_errors, growth_function, hypothesis_class_index, delta=.05):
+def shawe_taylor_bound(n_examples,
+                       n_errors,
+                       growth_function,
+                       errors_prob,
+                       complexity_prob,
+                       delta=.05,
+                       ):
     """
     Theorem 2.3 of Shawe-Taylor et al. (1997), Structural Risk Minimization over Data-Dependent Hierarchies, with the modification that Sauer's lemma is not used.
     """
-    p_d = 6/(np.pi**2*(hypothesis_class_index + 1))
-    q_k = 2**-(n_errors + 1)
-    # q_k = 6/(np.pi**2*(n_errors + 1))
-    epsilon = 2*n_errors + 4*np.log(float(growth_function(2*n_examples))) + 4*np.log(4/(p_d*q_k*delta))
+    epsilon = 2*n_errors + 4*np.log(float(growth_function(2*n_examples))) + 4*np.log(4/(complexity_prob*errors_prob*delta))
     return epsilon / n_examples
 
-def shawe_taylor_bound_pruning_objective_factory(n_features, table={}, loose_pfub=True):
+def shawe_taylor_bound_pruning_objective_factory(n_features,
+                                                 table={},
+                                                 loose_pfub=True,
+                                                 errors_prob_prior=None,
+                                                 complexity_prob_prior=None,
+                                                 delta=.05):
+    if errors_prob_prior is None:
+        r = 1/2
+        errors_prob_prior = lambda n_errors: (1-r) * r**n_errors
+        
+    if complexity_prob_prior is None:
+        s = 2
+        errors_prob_prior = lambda complexity_idx: 1 / (zeta(s) * (complexity_idx + 1)**s)
+        
     def shawe_taylor_bound_pruning_objective(subtree):
         copy_of_tree = copy(subtree.root)
         copy_of_subtree = copy_of_tree.follow_path(subtree.path_from_root())
@@ -23,26 +41,44 @@ def shawe_taylor_bound_pruning_objective_factory(n_features, table={}, loose_pfu
         growth_function = growth_function_upper_bound(copy_of_tree, n_features, n_classes, table, loose_pfub)
         n_examples = copy_of_tree.n_examples
         n_errors = copy_of_tree.n_errors
-        hypothesis_class_index = copy_of_tree.hash_value
+        errors_prob = errors_prob_prior(n_errors)
+        complexity_prob = complexity_prob_prior(copy_of_tree.hash_value)
         
-        return shawe_taylor_bound(n_examples, n_errors, growth_function, hypothesis_class_index)
+        return shawe_taylor_bound(n_examples, n_errors, growth_function, errors_prob, complexity_prob, delta)
+
     return shawe_taylor_bound_pruning_objective
 
 
-def vapnik_bound(n_examples, n_errors, growth_function, hypothesis_class_index, delta=.05):
+def vapnik_bound(n_examples,
+                 n_errors,
+                 growth_function,
+                 errors_prob,
+                 complexity_prob,
+                 delta=.05,
+                 ):
     """
     Equation (4.41) of Vapnik's book (1998) extended to SRM.
     """
-    p_d = 6/(np.pi**2*(hypothesis_class_index + 1))
-    q_k = 2**-(n_errors + 1)
-    # q_k = 6/(np.pi**2*(n_errors + 1))
-    epsilon = 4 / n_examples * (np.log(float(growth_function(2*n_examples))) + np.log(4/(p_d*q_k*delta)))
+    epsilon = 4 / n_examples * (np.log(float(growth_function(2*n_examples))) + np.log(4/(errors_prob*complexity_prob*delta)))
     
     empirical_risk = n_errors / n_examples
     
     return empirical_risk + epsilon/2 * (1 + np.sqrt(1 + 4*empirical_risk/epsilon))
 
-def vapnik_bound_pruning_objective_factory(n_features, table={}, loose_pfub=True):
+def vapnik_bound_pruning_objective_factory(n_features,
+                                           table={},
+                                           loose_pfub=True,
+                                           errors_prob_prior=None,
+                                           complexity_prob_prior=None,
+                                           delta=.05):
+    if errors_prob_prior is None:
+        r = 1/2
+        errors_prob_prior = lambda n_errors: (1-r) * r**n_errors
+        
+    if complexity_prob_prior is None:
+        s = 2
+        errors_prob_prior = lambda complexity_idx: 1 / (zeta(s) * (complexity_idx + 1)**s)
+        
     def vapnik_bound_pruning_objective(subtree):
         copy_of_tree = copy(subtree.root)
         copy_of_subtree = copy_of_tree.follow_path(subtree.path_from_root())
@@ -52,7 +88,8 @@ def vapnik_bound_pruning_objective_factory(n_features, table={}, loose_pfub=True
         growth_function = growth_function_upper_bound(copy_of_tree, n_features, n_classes, table, loose_pfub)
         n_examples = copy_of_tree.n_examples
         n_errors = copy_of_tree.n_errors
-        hypothesis_class_index = copy_of_tree.hash_value
+        errors_prob = errors_prob_prior(n_errors)
+        complexity_prob = complexity_prob_prior(copy_of_tree.hash_value)
         
-        return vapnik_bound(n_examples, n_errors, growth_function, hypothesis_class_index)
+        return vapnik_bound(n_examples, n_errors, growth_function, errors_prob, complexity_prob, delta)
     return vapnik_bound_pruning_objective
