2bf6f640ff6c64349b7a726d3161628bc404f356
Jean-Samuel Leboeuf <jean-samuel.leboeuf@hotmail.com>
2020-05-21 15:09:31 -0400

    cross validate prior

--- experiments/scripts/cross_validate_errors_prior.py
+++ experiments/scripts/cross_validate_errors_prior.py
@@ -20,7 +20,7 @@ with Timer():
     dtc = DecisionTreeClassifier(gini_impurity_criterion)
     n_folds = 5
 
-    radii = [2**-i for i in range(1, 16+1, 3)]
+    exponent = [i for i in range(1, 22+1)]
 
     cv_dtc = [copy(dtc) for i in range(n_folds)]
 
@@ -33,11 +33,12 @@ with Timer():
         print(f'Fold {fold}, train acc', accuracy_score(y_tr, cv_dtc[fold].predict(X_tr)), 'test acc', accuracy_score(y_ts, cv_dtc[fold].predict(X_ts)))
         print(cv_dtc[fold].tree.n_leaves)
 
-    n_errors = [0] * len(radii)
-    for k, r in enumerate(radii):
+    n_errors = [0] * len(exponent)
+    for k, exp in enumerate(exponent):
+        r = 2**-exp
         bound = vapnik_bound_pruning_objective_factory(
                                 n_features,
-                                errors_prob_prior=lambda n_err: (1-r) * r**n_err)
+                                errors_logprob_prior=lambda n_err: np.log(1-r) + n_err * np.log(r))
         for tree, (fold, (tr_idx, ts_idx)) in zip(cv_dtc, enumerate(fold_idx)):
             X_tr, y_tr = X[tr_idx], y[tr_idx]
             X_ts, y_ts = X[ts_idx], y[ts_idx]
@@ -46,12 +47,12 @@ with Timer():
             prune_with_bound(copy_of_tree, bound)
             
             # print(f'Fold {fold}, train acc', accuracy_score(y_tr, copy_of_tree.predict(X_tr)), 'test acc', accuracy_score(y_ts, copy_of_tree.predict(X_ts)))
-            print(copy_of_tree.tree.n_leaves)
+            # print(copy_of_tree.tree.n_leaves)
             
             y_pred = copy_of_tree.predict(X_ts)
             n_errors[k] += zero_one_loss(y_true=y_ts, y_pred=y_pred, normalize=False)
         
-        print(f'Radius {r}:\t mean test accuracy: {1 - n_errors[k]/n_examples}.')
+        print(f'Radius 2**-{exp}:\t mean test accuracy: {1 - n_errors[k]/n_examples}.')
 
-    optimal_radii = radii[np.argmin(n_errors)]
-    print(f'\nBest radius: {optimal_radii}.')
+    optimal_exponent = exponent[np.argmin(n_errors)]
+    print(f'\nBest exponent: {optimal_exponent} (1/radius = {2**optimal_exponent}).')
