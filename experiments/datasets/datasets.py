f7ddfc66f3083e4d71e16b49cf3304c234778623
Jean-Samuel Leboeuf <jean-samuel.leboeuf@hotmail.com>
2020-05-22 14:52:05 -0400

    wio

---
+++ experiments/datasets/datasets.py
@@ -0,0 +1,19 @@
+
+
+def load_dataset(dataset_name):
+    return
+
+
+class Dataset:
+    def __init__(self):
+        pass
+
+
+class BreastCancerWisconsin(Dataset):
+    url = "https://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer-wisconsin/breast-cancer-wisconsin.data"
+    def __init__(self):
+        super().__init__()
+
+
+class d(Dataset):
+    url = "https://archive.ics.uci.edu/ml/machine-learning-databases/00252/pop_failures.dat"
