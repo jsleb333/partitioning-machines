0f937268a2a8e60528155c582dd3f80dbb01b547
Jean-Samuel Leboeuf <jean-samuel.leboeuf@hotmail.com>
2020-05-27 14:04:45 -0400

    prepare for launch

--- experiments/main.py
+++ experiments/main.py
@@ -21,7 +21,7 @@ def launch_experiment(dataset,
                       n_draws=10,
                       n_folds=10,
                       max_n_leaves=40,
-                      error_prior_exponent=13,
+                      error_prior_exponent=13.1,
                       exp_name=None):
     exp_name = exp_name if exp_name is not None else datetime.now().strftime("%Y-%m-%d_%Hh%Mm")
     
@@ -36,8 +36,8 @@ def launch_experiment(dataset,
     
     X, y = dataset.data, dataset.target
 
-    # exp_path = f'./experiments/results/{dataset.name}/test_date/'
-    exp_path = f'./experiments/results/{dataset.name}/{exp_name}/'
+    exp_path = f'./experiments/results/{dataset.name}/test_date/'
+    # exp_path = f'./experiments/results/{dataset.name}/{exp_name}/'
 
     os.makedirs(exp_path, exist_ok=True)
 
@@ -54,9 +54,13 @@ def launch_experiment(dataset,
     csv_writers = [csv.writer(file) for file in files]
 
     header = ['draw', 'seed', 'train_accuracy', 'test_accuracy', 'n_leaves', 'height']
+    header_vapnik = header + ['bound']
 
-    for file, writer in zip(files, csv_writers):
-        writer.writerow(header)
+    for i, (file, writer) in enumerate(zip(files, csv_writers)):
+        if i == 1:
+            writer.writerow(header_vapnik)
+        else:
+            writer.writerow(header)
         file.flush()
 
     for draw in range(n_draws):
@@ -66,16 +70,20 @@ def launch_experiment(dataset,
                                                 test_size=test_split_ratio,
                                                 random_state=seed)
         trees = train(X_tr, y_tr,
-                        n_folds=n_folds,
-                        max_n_leaves=max_n_leaves)
+                      n_folds=n_folds,
+                      max_n_leaves=max_n_leaves,
+                      error_prior_exponent=error_prior_exponent)
         
         for i, (tree, file, csv_writer) in enumerate(zip(trees, files, csv_writers)):
             acc_tr = accuracy_score(y_tr, tree.predict(X_tr))
             acc_ts = accuracy_score(y_ts, tree.predict(X_ts))
             leaves = tree.tree.n_leaves
             height = tree.tree.height
-            
-            csv_writer.writerow([draw, seed, acc_tr, acc_ts, leaves, height])
+            if i == 1:
+                bound = tree.bound_value
+                csv_writer.writerow([draw, seed, acc_tr, acc_ts, leaves, height, bound])
+            else:
+                csv_writer.writerow([draw, seed, acc_tr, acc_ts, leaves, height])
             file.flush()
 
     for file in files:
@@ -85,6 +93,6 @@ def launch_experiment(dataset,
 if __name__ == "__main__":
     # datasets = list(load_datasets())
     # for dataset in datasets[4:]:
-    for dataset in dataset_list[-3:]:
+    for dataset in dataset_list[:1]:
         with Timer(f'Dataset {dataset.name}'):
             launch_experiment(dataset.load())
