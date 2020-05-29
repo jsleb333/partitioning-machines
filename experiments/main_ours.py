43e09fbf910cf669a1e1459e663de3c0e4577ec5
Jean-Samuel Leboeuf <jean-samuel.leboeuf@hotmail.com>
2020-05-29 11:35:49 -0400

    split main into ours and all to be more efficient

--- experiments/main_ours.py
+++ experiments/main_ours.py
@@ -5,8 +5,12 @@ import sys, os
 sys.path.append(os.getcwd())
 from datetime import datetime
 import csv
+from time import time
 
 from graal_utils import Timer
+
+from partitioning_machines import DecisionTreeClassifier, gini_impurity_criterion, shawe_taylor_bound_pruning_objective_factory
+from experiments.pruning import prune_with_bound
 
 from datasets.datasets import load_datasets, dataset_list
 from train import train
@@ -40,60 +44,57 @@ def launch_experiment(dataset,
     with open(exp_path + 'exp_params.py', 'w') as file:
         file.write(f"exp_params = {exp_params}")
 
-    model_names = ['original_tree',
-                   'vapnik_tree',
-                   'breiman_tree',
-                   'modified_breiman_tree',
-                   ]
+    model_name = 'ours'
 
-    files = [open(exp_path + name + '.csv', 'w', newline='') for name in model_names]
-    csv_writers = [csv.writer(file) for file in files]
+    file = open(exp_path + model_name + '.csv', 'w', newline='')
+    csv_writer = csv.writer(file)
 
-    header = ['draw', 'seed', 'train_accuracy', 'test_accuracy', 'n_leaves', 'height']
-    header_vapnik = header + ['bound']
+    header = ['draw', 'seed', 'train_accuracy', 'test_accuracy', 'n_leaves', 'height', 'bound', 'time']
 
-    for i, (file, writer) in enumerate(zip(files, csv_writers)):
-        if i == 1:
-            writer.writerow(header_vapnik)
-        else:
-            writer.writerow(header)
-        file.flush()
+    csv_writer.writerow(header)
+    file.flush()
 
     for draw in range(n_draws):
         print(f'Running draw #{draw}')
+        
         seed = draw*10 + 1
         X_tr, X_ts, y_tr, y_ts = train_test_split(X, y,
                                                 test_size=test_split_ratio,
                                                 random_state=seed)
-        trees = train(X_tr, y_tr,
-                      n_folds=n_folds,
-                      max_n_leaves=max_n_leaves,
-                      error_prior_exponent=error_prior_exponent)
-        
-        for i, (tree, file, csv_writer) in enumerate(zip(trees, files, csv_writers)):
-            acc_tr = accuracy_score(y_tr, tree.predict(X_tr))
-            acc_ts = accuracy_score(y_ts, tree.predict(X_ts))
-            leaves = tree.tree.n_leaves
-            height = tree.tree.height
-            if i == 1:
-                bound = tree.bound_value
-                csv_writer.writerow([draw, seed, acc_tr, acc_ts, leaves, height, bound])
-            else:
-                csv_writer.writerow([draw, seed, acc_tr, acc_ts, leaves, height])
-            file.flush()
 
-    for file in files:
-        file.close()
+        decision_tree = DecisionTreeClassifier(gini_impurity_criterion, max_n_leaves=max_n_leaves)
+        n_examples, n_features = X.shape
+        r = 1/2**error_prior_exponent
+        errors_logprob_prior = lambda n_err: np.log(1-r) + n_err * np.log(r)
+        bound = shawe_taylor_bound_pruning_objective_factory(n_features, errors_logprob_prior=errors_logprob_prior)
+    
+        decision_tree.fit(X_tr, y_tr)
+        print(decision_tree.tree.n_leaves)
+
+        t_start = time()
+        decision_tree.bound_value = prune_with_bound(decision_tree, bound)
+        elapsed_time = time() - t_start
+        print(decision_tree.tree.n_leaves)
+        
+        acc_tr = accuracy_score(y_tr, decision_tree.predict(X_tr))
+        acc_ts = accuracy_score(y_ts, decision_tree.predict(X_ts))
+        leaves = decision_tree.tree.n_leaves
+        height = decision_tree.tree.height
+        bound = decision_tree.bound_value
+        csv_writer.writerow([draw, seed, acc_tr, acc_ts, leaves, height, bound, elapsed_time])
+        file.flush()
+
+    file.close()
 
 
 if __name__ == "__main__":
     
-    exp_name = 'shawe-taylor_bound'
+    exp_name = 'debug'
     
     # for dataset in load_datasets(['iris', 'wine']):
     datasets = list(load_datasets())
-    for dataset in datasets[1:]:
+    for dataset in datasets:
         with Timer(f'Dataset {dataset.name}'):
             launch_experiment(dataset,
-                              error_prior_exponent=12.88,
+                              error_prior_exponent=13.7,
                               exp_name=exp_name)
