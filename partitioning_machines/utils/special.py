d40956a72b176d2c33314b5b26bf009c6ad5dbad
Jean-Samuel Leboeuf <jean-samuel.leboeuf@hotmail.com>
2020-05-29 09:59:36 -0400

    add a cache to WE for major speed up

--- partitioning_machines/utils/special.py
+++ partitioning_machines/utils/special.py
@@ -1,4 +1,7 @@
+import os
+from importlib import reload
 
+import partitioning_machines.utils.wedderburn_etherington_cache
 
 def wedderburn_etherington(n):
     """
@@ -6,20 +9,24 @@ def wedderburn_etherington(n):
     Args:
         n (int): The number of leaves of the binary tree.
     """
-    if n == 0:
-        return 0
-    if n == 1:
-        return 1
-
-    if n % 2 == 1:
+    cache = partitioning_machines.utils.wedderburn_etherington_cache.cache
+    if n in cache:
+        return cache[n]
+    
+    else:
         val = 0
-        for i in range(1, n//2+1):
-            val += wedderburn_etherington(i) * wedderburn_etherington(n-i)
-        return val
+        if n % 2 == 1:
+            for i in range(1, n//2+1):
+                val += wedderburn_etherington(i) * wedderburn_etherington(n-i)
+        else:
+            for i in range(1, n//2):
+                val += wedderburn_etherington(i) * wedderburn_etherington(n-i)
+            val += wedderburn_etherington(n//2)*(wedderburn_etherington(n//2) + 1)//2
 
-    if n % 2 == 0:
-        val = 0
-        for i in range(1, n//2):
-            val += wedderburn_etherington(i) * wedderburn_etherington(n-i)
-        val += wedderburn_etherington(n//2)*(wedderburn_etherington(n//2) + 1)//2
+        cache[n] = val
+        
+        with open(os.path.dirname(__file__) + '/wedderburn_etherington_cache.py', 'w') as file:
+            file.write(f"cache = {cache}")
+        reload(partitioning_machines.utils.wedderburn_etherington_cache)
+        
         return val
--- tests/test_utils/test_special.py
+++ tests/test_utils/test_special.py
@@ -7,3 +7,4 @@ def test_wedderburn_etherington():
     assert wedderburn_etherington(2) == 1
     answers = [0, 1, 1, 1, 2, 3, 6, 11, 23, 46, 98, 207, 451, 983, 2179, 4850, 10905, 24631, 56011]
     assert [wedderburn_etherington(i) for i in range(len(answers))] == answers
+    assert wedderburn_etherington(40)
