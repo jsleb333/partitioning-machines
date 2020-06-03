32bdf20ccd298303ec4c93620ce274f75a26a74c
Jean-Samuel Leboeuf <jean-samuel.leboeuf@hotmail.com>
2020-06-03 13:48:38 -0400

    exp can now be launch entirely via cmd

--- experiments/main.py
+++ experiments/main.py
@@ -11,31 +11,21 @@ from graal_utils import Timer
 
 from partitioning_machines import DecisionTreeClassifier, gini_impurity_criterion, shawe_taylor_bound_pruning_objective_factory, breiman_alpha_pruning_objective, modified_breiman_pruning_objective_factory
 from experiments.pruning import prune_with_bound, prune_with_cv
+from partitioning_machines import func_to_cmd
 
 from datasets.datasets import load_datasets, dataset_list
 from train import train
 
 
-def launch_experiment(dataset,
-                      model_name,
-                      test_split_ratio=.25,
-                      n_draws=25,
-                      n_folds=10,
-                      max_n_leaves=40,
-                      error_prior_exponent=13.1,
-                      exp_name=None):
-    """
-    Args:
-        dataset (Dataset object): The dataset object used in the experiment.
-        model_name (str): Valid model names are 'original', 'cart', 'm-cart' and 'ours'.
-        test_split_ratio (float): Ratio of examples that will be kept for test,
-        n_draws (int): Number of times the experiments will be run with a new random state.
-        n_folds (int): Number of folds used by the pruning algorithms of CART and M-CART. (Ignored by 'ours' algorithm).
-        max_n_leaves (int): Maximum number of leaves the original tree is allowed to have.
-        error_prior_exponent (int): The distribution q_k will be of the form (1-r) * r**k, where r = 2**(-error_prior_exponent). (Ignored by 'cart' and 'm-cart' algorithms).
-        exp_name (str): Name of the experiment. Will be used to save the results on disk.
-    """
-    exp_name = exp_name if exp_name is not None else datetime.now().strftime("%Y-%m-%d_%Hh%Mm")
+def launch_single_experiment(dataset,
+                             model_name,
+                             exp_name,
+                             test_split_ratio=.25,
+                             n_draws=25,
+                             n_folds=10,
+                             max_n_leaves=40,
+                             error_prior_exponent=13.1,
+                             ):
     
     exp_params = {
         'exp_name':exp_name,
@@ -63,9 +53,13 @@ def launch_experiment(dataset,
 
     csv_writer.writerow(header)
     file.flush()
+    
+    times_per_draw = []
 
     for draw in range(n_draws):
-        print(f'Running draw #{draw}')
+        time_str = f'\tMean time per draw: {sum(times_per_draw)/len(times_per_draw):.3f}s.' if times_per_draw else ''
+        print(f'Running draw #{draw:02d}...' + time_str, end='\r')
+        draw_start = time()
         
         seed = draw*10 + 1
         X_tr, X_ts, y_tr, y_ts = train_test_split(X, y,
@@ -92,6 +86,8 @@ def launch_experiment(dataset,
         elif model_name == 'm-cart':
             modified_breiman_pruning_objective = modified_breiman_pruning_objective_factory(n_features)
             prune_with_cv(decision_tree, X_tr, y_tr, n_folds=n_folds,     pruning_objective=modified_breiman_pruning_objective)
+        elif model_name != 'original':
+            raise ValueError(f"Unknown model {model_name}. Valid models are 'original', 'cart', 'm-cart' and 'ours'. ")
         
         elapsed_time = time() - t_start
 
@@ -102,22 +98,68 @@ def launch_experiment(dataset,
         bound = decision_tree.bound_value
         csv_writer.writerow([draw, seed, acc_tr, acc_ts, leaves, height, bound, elapsed_time])
         file.flush()
+        
+        times_per_draw.append(time() - draw_start)
 
+    print(f'\rCompleted all {draw+1} draws.')
     file.close()
 
 
+@func_to_cmd
+def launch_experiment(dataset=list(),
+                      model_names='all',
+                      exp_name='',
+                      test_split_ratio=.25,
+                      n_draws=25,
+                      n_folds=10,
+                      max_n_leaves=40,
+                      error_prior_exponent=13.1,
+                      ):
+    """
+    Will launch experiment with specified parameters. Automatically saves all results in the file: "./experiments/results/<dataset_name>/<exp_name>/<model_name>.csv". Experiments parameters are saved in the file: "./experiments/results/<dataset_name>/<exp_name>/<model_name>_params.py".
+    
+    Args:
+        dataset (list[str]): The dataset name to be used in the experiment. By default (an empty list) will iterate over all available datasets. Otherwise, will launch experiment for the specified datasets. To see all available datasets, consult the file "./experiments/datasets/datasets.py".
+        
+        model_names (str): Valid model names are 'original', 'cart', 'm-cart' and 'ours'. By default 'all' will run all 4 models one after the other.
+        
+        exp_name (str): Name of the experiment. Will be used to save the results on disk. If empty, the date and time of the beginning of the experiment will be used.
+
+        test_split_ratio (float): Ratio of examples that will be kept for test.
+        
+        n_draws (int): Number of times the experiments will be run with a new random state.
+        
+        n_folds (int): Number of folds used by the pruning algorithms of CART and M-CART. (Ignored by 'ours' algorithm).
+        
+        max_n_leaves (int): Maximum number of leaves the original tree is allowed to have.
+        
+        error_prior_exponent (int): The distribution q_k will be of the form (1-r) * r**k, where r = 2**(-error_prior_exponent). (Ignored by 'cart' and 'm-cart' algorithms).
+    """
+    if not dataset:
+        datasets = list(load_datasets())
+    else:
+        datasets = list(load_datasets(dataset))
+    
+    if model_names == 'all':
+        model_names = ['original', 'ours', 'cart', 'm-cart']
+    else:
+        model_names = [model_names]
+        
+    if not exp_name:
+        exp_name = exp_name if exp_name else datetime.now().strftime("%Y-%m-%d_%Hh%Mm")
+    
+    for model_name in model_names:
+        for dataset in datasets:
+            with Timer(f'{model_name} model on dataset {dataset.name}'):
+                launch_single_experiment(dataset=dataset,
+                                         model_name=model_name,
+                                         exp_name=exp_name,
+                                         test_split_ratio=test_split_ratio,
+                                         n_draws=n_draws,
+                                         n_folds=n_folds,
+                                         max_n_leaves=max_n_leaves,
+                                         error_prior_exponent=error_prior_exponent,
+                                         )
+
 if __name__ == "__main__":
-    
-    exp_name = 'with_time'
-    
-    # datasets = list(load_datasets(['iris', 'wine']))
-    datasets = list(load_datasets())
-    for dataset in datasets:
-        with Timer(f'Dataset {dataset.name}'):
-            launch_experiment(dataset=dataset,
-                              model_name='cart',
-                              error_prior_exponent=13.7,
-                              exp_name=exp_name,
-                              n_draws=25,
-                              max_n_leaves=40,
-                              test_split_ratio=.25)
+    launch_experiment()
