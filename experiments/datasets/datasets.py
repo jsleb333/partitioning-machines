fa35434b4c0721ca82908f76e36245b232756e10
Jean-Samuel Leboeuf <jean-samuel.leboeuf@hotmail.com>
2021-09-08 17:19:47 -0400

    refactor dataset metaclass and added two datasets

--- experiments/datasets/datasets.py
+++ experiments/datasets/datasets.py
@@ -1,4 +1,6 @@
 import os, sys
+
+from pandas.core.indexes import category
 sys.path.append(os.getcwd())
 from urllib import request
 import pandas as pd
@@ -6,19 +8,18 @@ import numpy as np
 from zipfile import ZipFile
 import re
 
-dataset_list = []
 
+dataset_list = []
 
 def load_datasets(datasets=None):
     """
     Args:
         datasets (list of str): Lists of datasets to load by name. If None, all available datasets are loaded iteratively.
     """
-    if datasets is None:
-        datasets = [d.name for d in dataset_list]
     for dataset in dataset_list:
-        if dataset.name in datasets:
-            yield dataset.load()
+        if datasets is not None and dataset.name in datasets:
+            yield dataset
+            dataset.unload()
 
 
 def camel_to_snake(name):
@@ -26,35 +27,56 @@ def camel_to_snake(name):
     return re.sub('([a-z0-9])([A-Z])', r'\1_\2', name).lower()
 
 
-class Dataset(type):
+class LazyDataset(type):
+    """Dataset metaclass which makes every datasets implemented 'lazy' in the sense that it will be loaded in memory only when its attributes are accessed. Each dataset is a singleton and can be unloaded from memory using the 'unload' method.
+    """
     def __init__(cls, cls_name, bases, attrs):
-        cls.dataset_name = camel_to_snake(cls_name)
-        cls._dataframe = None
-        # Each dataset is a singleton, but initializing will load into memory the dataset
-        def cls_new(self, *args, **kwargs):
-            if cls._dataframe is None:
-                cls.load()
-            return cls
-        cls.__new__ = cls_new
-
-        cls.build_dataframe = classmethod(cls.build_dataframe) # Make the method accessible to the class and not only the instance
-
+        super().__init__(cls, bases, attrs)
+        cls.name = camel_to_snake(cls_name)
+        cls.path_to_raw_file = os.path.dirname(__file__) + '/raw/' + cls.name + '.raw'
+        cls._is_loaded = False
         dataset_list.append(cls) # Append to the list of available datasets
 
-        super().__init__(cls, bases, attrs)
-
-    def __repr__(cls):
-        return f'Dataset {cls.dataset_name} with {cls.n_examples} examples and {cls.n_features} features'
-
-    @property
-    def path_to_raw_file(cls):
-        return os.path.dirname(__file__) + '/raw/' + cls.dataset_name + '.raw'
-
     def load(cls):
+        cls._is_loaded == True
+        cls.nominal_features = []
         if not os.path.exists(cls.path_to_raw_file):
             cls.download_dataset()
+        cls.dataframe = cls.build_dataframe(cls)
+        cls.data = cls.dataframe.loc[:, cls.dataframe.columns != 'class'].to_numpy()
+        cls.target = cls.dataframe.loc[:, 'class'].to_numpy()
+        cls.n_examples = cls.data.shape[0]
+        cls.n_features = cls.data.shape[1]
+        cls.n_classes = len(set(cls.target))
+        cls.nominal_feat_dist = cls._compute_nominal_feature_distribution()
+        return cls
 
-        cls.dataframe = cls.build_dataframe()
+    def _compute_nominal_feature_distribution(cls):
+        tmp_cat_dist = [len(cls.dataframe.iloc[:,i].cat.categories) for i in cls.nominal_features]
+        nominal_feat_dist = [0]*max(tmp_cat_dist)
+        for n_nom_feat in tmp_cat_dist:
+            nominal_feat_dist[n_nom_feat-1] += 1
+        return nominal_feat_dist
+
+    def unload(cls):
+        del cls.dataframe
+        del cls.data
+        del cls.target
+        cls._is_loaded = False
+
+    def __call__(cls, *args, **kwds): # Makes the dataset a singleton
+        if not cls._is_loaded:
+            cls.load()
+        return cls
+
+    def __getattr__(cls, attr): # Loads the dataset on-demand (i.e. lazily)
+        if not cls._is_loaded:
+            cls.load()
+            return getattr(cls, attr)
+        raise AttributeError
+
+    def __repr__(cls):
+        return f'Dataset {cls.name} with {cls.n_examples} examples and {cls.n_features} features'
 
     def download_dataset(cls):
         content = request.urlopen(cls.url)
@@ -63,41 +85,40 @@ class Dataset(type):
             for line in content:
                 file.write(line)
 
-    def build_dataframe(cls):
+    def build_dataframe(cls) -> pd.DataFrame:
         raise NotImplementedError
 
-    @property
-    def dataframe(cls):
-        if cls._dataframe is None:
-            cls.load()
-        return cls._dataframe
 
-    @dataframe.setter
-    def dataframe(cls, value):
-        cls._dataframe = value
+class Adult(metaclass=LazyDataset):
+    url = "https://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.data"
+    bibtex_label = "kohavi1996scaling"
+    def build_dataframe(cls):
+        with open(cls.path_to_raw_file, 'r') as file:
+            features = {
+                'age': float,
+                'workclass': 'category',
+                'fnlwgt': float,
+                'education': 'category',
+                'education-num': float,
+                'marital-status': 'category',
+                'occupation': 'category',
+                'relationship': 'category',
+                'race': 'category',
+                'sex': 'category',
+                'capital-gain': float,
+                'capital-loss': float,
+                'hours-per-week': float,
+                'native-country': 'category',
+                'class': 'category'
+            }
+            cls.nominal_features = [i for i, v in enumerate(features.values()) if v == 'category']
+            df = pd.read_csv(file, names=features.keys(), header=None, dtype=features)
+            for col_name, dtype in features.items():
+                if dtype == 'category':
+                    df[col_name].cat.categories = list(i for i, _ in enumerate(set(df[col_name])))
+        return df
 
-    @property
-    def data(cls):
-        return cls.dataframe.loc[:, cls.dataframe.columns != 'class'].to_numpy(dtype=float)
-
-    @property
-    def target(cls):
-        return cls.dataframe.loc[:, 'class'].to_numpy()
-
-    @property
-    def n_examples(cls):
-        return cls.data.shape[0]
-
-    @property
-    def n_features(cls):
-        return cls.data.shape[1]
-
-    @property
-    def n_classes(cls):
-        return len(set(cls.target))
-
-
-class BreastCancerWisconsinDiagnostic(metaclass=Dataset):
+class BreastCancerWisconsinDiagnostic(metaclass=LazyDataset):
     url = "https://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer-wisconsin/wdbc.data"
     bibtex_label = "street1993nuclear"
     def build_dataframe(cls):
@@ -107,7 +128,7 @@ class BreastCancerWisconsinDiagnostic(metaclass=Dataset):
             df.drop(columns=col_names[0], inplace=True)
         return df
 
-class Cardiotocography10(metaclass=Dataset):
+class Cardiotocography10(metaclass=LazyDataset):
     url = "https://archive.ics.uci.edu/ml/machine-learning-databases/00193/CTG.xls"
     bibtex_label = "ayres2000sisporto"
     def build_dataframe(cls):
@@ -119,7 +140,7 @@ class Cardiotocography10(metaclass=Dataset):
             df.rename(columns={'CLASS':'class'}, inplace=True)
         return df
 
-class ClimateModelSimulationCrashes(metaclass=Dataset):
+class ClimateModelSimulationCrashes(metaclass=LazyDataset):
     url = "https://archive.ics.uci.edu/ml/machine-learning-databases/00252/pop_failures.dat"
     bibtex_label = "lucas2013failure"
     def build_dataframe(cls):
@@ -129,7 +150,7 @@ class ClimateModelSimulationCrashes(metaclass=Dataset):
             df.rename(columns={list(df)[-1]:'class'}, inplace=True)
         return df
 
-class ConnectionistBenchSonar(metaclass=Dataset):
+class ConnectionistBenchSonar(metaclass=LazyDataset):
     url = "https://archive.ics.uci.edu/ml/machine-learning-databases/undocumented/connectionist-bench/sonar/sonar.all-data"
     bibtex_label = "gorman1988analysis"
     def build_dataframe(cls):
@@ -138,7 +159,7 @@ class ConnectionistBenchSonar(metaclass=Dataset):
             df.rename(columns={list(df)[-1]:'class'}, inplace=True)
         return df
 
-class DiabeticRetinopathyDebrecen(metaclass=Dataset):
+class DiabeticRetinopathyDebrecen(metaclass=LazyDataset):
     url = "https://archive.ics.uci.edu/ml/machine-learning-databases/00329/messidor_features.arff"
     bibtex_label = "antal2014ensemble"
     def build_dataframe(cls):
@@ -147,7 +168,7 @@ class DiabeticRetinopathyDebrecen(metaclass=Dataset):
             df.rename(columns={list(df)[-1]:'class'}, inplace=True)
         return df
 
-class Fertility(metaclass=Dataset):
+class Fertility(metaclass=LazyDataset):
     url = "https://archive.ics.uci.edu/ml/machine-learning-databases/00244/fertility_Diagnosis.txt"
     bibtex_label = "gil2012predicting"
     def build_dataframe(cls):
@@ -156,7 +177,7 @@ class Fertility(metaclass=Dataset):
             df.rename(columns={list(df)[-1]:'class'}, inplace=True)
         return df
 
-class HabermansSurvival(metaclass=Dataset):
+class HabermansSurvival(metaclass=LazyDataset):
     url = "https://archive.ics.uci.edu/ml/machine-learning-databases/haberman/haberman.data"
     bibtex_label = "haberman1976generalized"
     def build_dataframe(cls):
@@ -165,7 +186,35 @@ class HabermansSurvival(metaclass=Dataset):
             df.rename(columns={list(df)[-1]:'class'}, inplace=True)
         return df
 
-class ImageSegmentation(metaclass=Dataset):
+class HeartDiseaseClevelandProcessed(metaclass=LazyDataset):
+    url = "https://archive.ics.uci.edu/ml/machine-learning-databases/heart-disease/processed.cleveland.data"
+    bibtex_label = "detrano1989international"
+    def build_dataframe(cls):
+        with open(cls.path_to_raw_file, 'r') as file:
+            features = {
+                'age': float,
+                'sex': 'category',
+                'cp': 'category',
+                'trestbps': float,
+                'chol': float,
+                'fbs': float,
+                'restecg': 'category',
+                'thalach': float,
+                'exang': 'category',
+                'oldpeak': float,
+                'slope': 'category',
+                'ca': 'category',
+                'thal': 'category',
+                'class': 'category'
+            }
+            cls.nominal_features = [i for i, v in enumerate(features.values()) if v == 'category']
+            df = pd.read_csv(file, names=features.keys(), header=None, dtype=features)
+            for col_name, dtype in features.items():
+                if dtype == 'category':
+                    df[col_name].cat.categories = list(i for i, _ in enumerate(set(df[col_name])))
+        return df
+
+class ImageSegmentation(metaclass=LazyDataset):
     url = "https://archive.ics.uci.edu/ml/machine-learning-databases/image/segmentation.data"
     bibtex_label = None
     def build_dataframe(cls):
@@ -174,7 +223,7 @@ class ImageSegmentation(metaclass=Dataset):
             df.rename(columns={list(df)[0]:'class'}, inplace=True)
         return df
 
-class Ionosphere(metaclass=Dataset):
+class Ionosphere(metaclass=LazyDataset):
     url = "https://archive.ics.uci.edu/ml/machine-learning-databases/ionosphere/ionosphere.data"
     bibtex_label = "sigillito1989classification"
     def build_dataframe(cls):
@@ -183,7 +232,7 @@ class Ionosphere(metaclass=Dataset):
             df.rename(columns={list(df)[-1]:'class'}, inplace=True)
         return df
 
-class Iris(metaclass=Dataset):
+class Iris(metaclass=LazyDataset):
     url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
     bibtex_label = "fisher1936use"
     def build_dataframe(cls):
@@ -191,7 +240,7 @@ class Iris(metaclass=Dataset):
             df = pd.read_csv(file, header=None, names=['sepal length', 'sepal width', 'petal length', 'petal width', 'class'])
         return df
 
-class Parkinson(metaclass=Dataset):
+class Parkinson(metaclass=LazyDataset):
     url = "https://archive.ics.uci.edu/ml/machine-learning-databases/parkinsons/parkinsons.data"
     bibtex_label = "little2007exploiting"
     def build_dataframe(cls):
@@ -201,7 +250,7 @@ class Parkinson(metaclass=Dataset):
             df.drop(columns='name', inplace=True)
         return df
 
-class PlanningRelax(metaclass=Dataset):
+class PlanningRelax(metaclass=LazyDataset):
     url = "https://archive.ics.uci.edu/ml/machine-learning-databases/00230/plrx.txt"
     bibtex_label = "bhatt2012planning"
     def build_dataframe(cls):
@@ -211,7 +260,7 @@ class PlanningRelax(metaclass=Dataset):
             df.rename(columns={list(df)[-1]:'class'}, inplace=True)
         return df
 
-class QSARBiodegradation(metaclass=Dataset):
+class QSARBiodegradation(metaclass=LazyDataset):
     url = "https://archive.ics.uci.edu/ml/machine-learning-databases/00254/biodeg.csv"
     bibtex_label = "mansouri2013quantitative"
     def build_dataframe(cls):
@@ -220,7 +269,7 @@ class QSARBiodegradation(metaclass=Dataset):
             df.rename(columns={list(df)[-1]:'class'}, inplace=True)
         return df
 
-class Seeds(metaclass=Dataset):
+class Seeds(metaclass=LazyDataset):
     url = "https://archive.ics.uci.edu/ml/machine-learning-databases/00236/seeds_dataset.txt"
     bibtex_label = "charytanowicz2010complete"
     def build_dataframe(cls):
@@ -229,7 +278,7 @@ class Seeds(metaclass=Dataset):
             df.rename(columns={list(df)[-1]:'class'}, inplace=True)
         return df
 
-class Spambase(metaclass=Dataset):
+class Spambase(metaclass=LazyDataset):
     url = "https://archive.ics.uci.edu/ml/machine-learning-databases/spambase/spambase.data"
     bibtex_label = None
     def build_dataframe(cls):
@@ -238,7 +287,7 @@ class Spambase(metaclass=Dataset):
             df.rename(columns={list(df)[-1]:'class'}, inplace=True)
         return df
 
-class VertebralColumn3C(metaclass=Dataset):
+class VertebralColumn3C(metaclass=LazyDataset):
     url = "https://archive.ics.uci.edu/ml/machine-learning-databases/00212/vertebral_column_data.zip"
     bibtex_label = "berthonnaud2005analysis"
     def build_dataframe(cls):
@@ -248,7 +297,7 @@ class VertebralColumn3C(metaclass=Dataset):
                 df.rename(columns={list(df)[-1]:'class'}, inplace=True)
         return df
 
-class WallFollowingRobot24(metaclass=Dataset):
+class WallFollowingRobot24(metaclass=LazyDataset):
     url = "https://archive.ics.uci.edu/ml/machine-learning-databases/00194/sensor_readings_24.data"
     bibtex_label = "freire2009short"
     def build_dataframe(cls):
@@ -257,7 +306,7 @@ class WallFollowingRobot24(metaclass=Dataset):
             df.rename(columns={list(df)[-1]:'class'}, inplace=True)
         return df
 
-class Wine(metaclass=Dataset):
+class Wine(metaclass=LazyDataset):
     url = "https://archive.ics.uci.edu/ml/machine-learning-databases/wine/wine.data"
     bibtex_label = "aeberhard1994comparative"
     def build_dataframe(cls):
@@ -281,7 +330,7 @@ class Wine(metaclass=Dataset):
             df = pd.read_csv(file, names=col_names, header=None)
         return df
 
-class Yeast(metaclass=Dataset):
+class Yeast(metaclass=LazyDataset):
     url = "https://archive.ics.uci.edu/ml/machine-learning-databases/yeast/yeast.data"
     bibtex_label = "horton1996probabilistic"
     def build_dataframe(cls):
@@ -293,9 +342,9 @@ class Yeast(metaclass=Dataset):
 
 
 if __name__ == "__main__":
-    for i, d in enumerate(dataset_list):
+    for i, d in enumerate(load_datasets(['heart_disease_cleveland_processed'])):
         print(d)
         assert not np.isnan(d.data.sum())
-        print(i, d.dataset_name, d.n_examples, d.n_classes)
+        print(i, d.name, d.nominal_features, d.nominal_feat_dist)
 
 
