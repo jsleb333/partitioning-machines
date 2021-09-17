b5238ce5068d308f3f58f6d3e2b2926ac8768dd7
Jean-Samuel Leboeuf <jean-samuel.leboeuf@hotmail.com>
2021-09-17 13:10:23 -0400

    add filter_signature func

--- experiments/utils.py
+++ experiments/utils.py
@@ -1,6 +1,22 @@
 import re
+from inspect import signature
 
 
 def camel_to_snake(name):
     name = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
     return re.sub('([a-z0-9])([A-Z])', r'\1_\2', name).lower()
+
+
+def filter_signature(cls):
+    sig = {}
+    for base_cls in cls.mro():
+        if base_cls is not object:
+            sig |= signature(base_cls.__init__).parameters
+
+    def init_cls(*args, **kwargs):
+        for kw in list(kwargs):
+            if kw not in sig:
+                del kwargs[kw]
+        return cls(*args, **kwargs)
+
+    return init_cls
