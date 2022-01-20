6b6ba83d1f20a05ad0f5df863b49722ab9a23df9
Jean-Samuel Leboeuf <jean-samuel.leboeuf@hotmail.com>
2022-01-20 16:37:30 -0500

    wip bug fixes

--- experiments/scripts/comp_reduced_error.py
+++ experiments/scripts/comp_reduced_error.py
@@ -10,50 +10,54 @@ from experiments.datasets.datasets import QSARBiodegradation
 
 
 class NoPruningVal(NoPruning):
-    def __init__(self, *, val_split_ratio=.2, **kwargs) -> None:
+    def __init__(self, *, val_split_ratio=.2, seed=42, **kwargs) -> None:
         super().__init__(**kwargs)
         self.val_split_ratio = val_split_ratio
+        self.seed = seed
 
     def fit_tree(self, dataset) -> None:
-        dataset.make_train_val_split()
+        dataset.make_train_val_split(self.val_split_ratio, self.seed)
         super().fit_tree(dataset)
 
 
 class STPruningVal(OursShaweTaylorPruning):
-    def __init__(self, *, val_split_ratio=.2, **kwargs) -> None:
+    def __init__(self, *, val_split_ratio=.2, seed=42, **kwargs) -> None:
         super().__init__(**kwargs)
         self.val_split_ratio = val_split_ratio
+        self.seed = seed
 
     def fit_tree(self, dataset) -> None:
-        dataset.make_train_val_split()
+        dataset.make_train_val_split(self.val_split_ratio, self.seed)
         super().fit_tree(dataset)
 
 
 class OraclePruningVal(OraclePruning):
-    def __init__(self, *, val_split_ratio=.2, **kwargs) -> None:
+    def __init__(self, *, val_split_ratio=.2, seed=42, **kwargs) -> None:
         super().__init__(**kwargs)
         self.val_split_ratio = val_split_ratio
+        self.seed = seed
 
     def fit_tree(self, dataset) -> None:
-        dataset.make_train_val_split()
+        dataset.make_train_val_split(self.val_split_ratio, self.seed)
         super().fit_tree(dataset)
 
 
 if __name__ == "__main__":
 
-    dataset = QSARBiodegradation
+    dataset = QSARBiodegradation()
     exp_path = f'./experiments/results/reder-exp01/{dataset.name}/'
     n_draws = 10
     test_split_ratio = .2
+    seed = 42
     max_n_leaves = [10, 20, 30, 40, 50, 60]
     val_split_ratios = np.linspace(0,1,11)
     models = [NoPruningVal, STPruningVal, ReducedErrorPruning, OraclePruningVal]
 
-    # exp_path = f'./experiments/results/test/'
-    # n_draws = 1
-    # max_n_leaves = [50, 55, 60]
-    # val_split_ratios = [.2]
-    # models = [STPruningVal]
+    exp_path = f'./experiments/results/test/'
+    n_draws = 1
+    max_n_leaves = [10]
+    val_split_ratios = [.8]
+    models = [NoPruningVal]
 
     for model in models:
         for n_leaves in max_n_leaves:
@@ -68,6 +72,9 @@ if __name__ == "__main__":
                         model=model(val_split_ratio=val_split_ratio, max_n_leaves=n_leaves),
                         test_split_ratio=test_split_ratio,
                         n_draws=n_draws,
-                        exp_name=exp_name
+                        exp_name=exp_name,
+                        seed=seed,
                     ).run(logger=Logger(exp_path),
                           tracker=Tracker())
+
+                    print(dataset.train_size, dataset.val_size, dataset.test_size)
