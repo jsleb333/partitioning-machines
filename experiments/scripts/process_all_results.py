785f0b91d2ac49d876cc61cc384151d0247819a2
Jean-Samuel Leboeuf <jean-samuel.leboeuf@hotmail.com>
2022-11-16 15:07:52 -0500

    update process results for paper

--- experiments/scripts/process_all_results.py
+++ experiments/scripts/process_all_results.py
@@ -7,7 +7,6 @@ import numpy as np
 from scipy.stats import t as students_t
 
 from experiments.datasets import dataset_list as d_list
-from experiments.models import model_dict
 from experiments.utils import camel_to_snake
 
 from partitioning_machines import func_to_cmd
@@ -26,6 +25,7 @@ class MeanWithCI(float, p2l.TexObject):
         return self.std * students_t.ppf((1+confidence_level)/2, n_samples-1)
 
     def __format__(self, format_spec):
+        # return f'${format(self.mean, format_spec)} ({format(self.ci, format_spec)})$'
         # return f'${format(self.mean, format_spec)} \pm {format(self.ci, format_spec)}$'
         return f'${format(self.mean, format_spec)}$'
 
@@ -54,48 +54,57 @@ def process_results(exp_name='exp02'):
         exec(file.read().replace('"', ''), namespace)
         exp_config = namespace['exp_config']
 
-    caption = f"""Mean test accuracy and standard deviation on {exp_config['n_draws']} random splits of {len(dataset_list)} datasets taken from the UCI Machine Learning Repository \\citep{{Dua:2019}}. In parenthesis is the total number of examples followed by the number of classes of the dataset. The best performances up to a ${significance}\\%$ accuracy gap are highlighted in bold. The maximum number of leaves is set to {exp_config['model_config']['max_n_leaves']}. The dataset is split into training and test sets with a ratio of {exp_config['test_split_ratio']}."""
+    caption = f"""Mean test accuracy on {exp_config['n_draws']} random splits of {len(dataset_list)} data sets obtained from the UCI Machine Learning Repository \\citep{{Dua:2019}}. Starred data sets contain a mixture of feature types (the others are real-valued only). The total number of examples followed by the number of classes of the dataset is indicated in parenthesis. The best performances up to a ${significance}\\%$ accuracy gap are highlighted in bold. The maximum number of leaves is set to {exp_config['model_config']['max_n_leaves']}. The dataset is split into training and test sets with a ratio of {exp_config['test_split_ratio']}."""
 
     label = "results"
 
-    alignement = r'@{\hspace{6pt}}'.join('l'+'c'*len(model_dict))
+    models = {
+        'no_pruning': 'OG',
+        'cart_pruning': 'CC',
+        'reduced_error_pruning': 'RE',
+        'kearns_mansour_pruning': 'KM',
+        'ours_shawe_taylor_pruning': 'Ours',
+        'oracle_pruning': 'Oracle',
+    }
+
+    alignment = r'@{\hspace{6pt}}'.join('l'+'c'*len(models))
     table = doc.new(p2l.Table(
-        (len(dataset_list)+6, len(model_dict)+1),
+        (len(dataset_list)+7, len(models)+1),
         float_format='.2f',
-        alignment=alignement,
+        alignment=alignment,
         caption=caption,
         label=label
     ))
     table.body.insert(0, '\\small')
 
-    table[0:2,0].multicell('Dataset', v_shift='-3pt')
-    table[0,1:] = 'Model'
-    table[1,1:] = [name.replace('_', ' ')
-                   .title()
-                   .replace(' Pruning', '')
-                   .replace('Shawe Taylor', 'ST')
-                   .replace('Hyp Inv', 'HTI')
-                   .replace('Kearns Mansour', 'KM')
-                   for name in model_dict]
+    table[0:2,0].multicell('Data set', v_shift='-3pt')
+    table[0,1:] = 'Pruning model'
+    table[1,1:] = list(models.values())
     table[0,1:].add_rule()
     is_nominal = lambda d: '*' if d.nominal_features else ''
     table[2:len(dataset_list)+2,0] = [d.name.replace('_', ' ').title() + is_nominal(d) + f' ({d.n_examples}, {d.n_classes})' for d in dataset_list]
     table[1].add_rule()
-    table[-5].add_rule()
 
-    ts_accs = np.zeros((len(model_dict), len(dataset_list)))
+    ts_accs = np.zeros((len(models), len(dataset_list)))
+
+    times = {name:[] for name in models}
+    leaves = {name:[] for name in models}
 
     for d, dataset in enumerate(dataset_list):
-        for i, model_name in enumerate(model_dict):
+        for i, model_name in enumerate(models):
             ts_acc = []
             path = './experiments/results/' + exp_name + '/' + dataset.name + '/'
             try:
                 with open(path + model_name + '.csv', 'r', newline='') as file:
                     reader = csv.reader(file)
                     header = next(reader)
-                    pos = header.index('test_accuracy')
+                    pos_acc = header.index('test_accuracy')
+                    pos_time = header.index('time')
+                    pos_leaves = header.index('n_leaves')
                     for row in reader:
-                        ts_acc.append(row[pos])
+                        ts_acc.append(row[pos_acc])
+                        times[model_name].append(float(row[pos_time]))
+                        leaves[model_name].append(float(row[pos_leaves]))
                 table[d+2, i+1] = MeanWithCI(100*np.array(ts_acc, dtype=float))
 
             except:
@@ -104,46 +113,51 @@ def process_results(exp_name='exp02'):
             if ts_acc:
                 ts_accs[i,d] = np.mean(np.array(ts_acc, dtype=float))
 
-        table[d+2,2:-1].highlight_best(best=lambda content: '$\\mathbf{' + content[1:-1] + '}$', atol=significance, rtol=0)
+        table[d+2,1:-1].highlight_best(best=lambda content: '$\\mathbf{' + content[1:-1] + '}$', atol=significance, rtol=0)
 
-    table[-4,0] = r'Number of best'
+    n_d = len(dataset_list)+1
+
+    table[n_d].add_rule()
+
+    table[n_d+1,0] = r'Number of best'
     bests = np.isclose(ts_accs, np.max(ts_accs[:-1,:], axis=0), rtol=0, atol=significance/100)
-    table[-4,1:] = np.sum(bests, axis=1)
-    table[-3,0] = r'Better than Ours ST'
-    bests = np.zeros_like(ts_accs, dtype=int)
-    for d in range(len(dataset_list)):
-        b = ts_accs[1,d]
-        for i in range(len(model_dict)):
-            a = ts_accs[i,d]
-            if a > b and not np.isclose(a, b, rtol=0, atol=significance/100):
-                bests[i,d] = 1
-    table[-3,1:] = np.sum(bests, axis=1)
+    table[n_d+1,1:] = np.sum(bests, axis=1)
+    table[n_d+1,-1] = 'N/A'
 
-    table[-2,0] = 'Mean'
-    table[-2,1:] = [MeanWithCI(ts_accs[i]*100) for i in range(len(model_dict))]
-    table[-1,0] = 'Fraction of oracle'
-    table[-1,1:] = [MeanWithCI((1-ts_accs[-1]/ts_accs[i])*100) if (ts_accs[i] > 0).all() else 0 for i in range(len(model_dict))]
+    # table[-3,0] = r'Better than Ours ST'
+    # bests = np.zeros_like(ts_accs, dtype=int)
+    # for d in range(len(dataset_list)):
+    #     b = ts_accs[1,d]
+    #     for i in range(len(models)):
+    #         a = ts_accs[i,d]
+    #         if a > b and not np.isclose(a, b, rtol=0, atol=significance/100):
+    #             bests[i,d] = 1
+    # table[-3,1:] = np.sum(bests, axis=1)
+
+    table[n_d+2,0] = 'Mean accuracy (\\%)'
+    table[n_d+2,1:] = [MeanWithCI(ts_accs[i]*100).mean for i in range(len(models))]
+
+    table[n_d+3,0] = 'Mean fraction of Oracle (\\%)'
+    table[n_d+3,1:] = [MeanWithCI(100*(ts_accs[i]/ts_accs[-1])).mean if (ts_accs[i] > 0).all() else 0 for i in range(len(models))]
+
 
     d = [dataset_list[i] for i in [0, 2, 3, 4, 16]]
 
-    # table[2,0] = f'BCWD\\textsuperscript{{a}} ({d[0].n_examples}, {d[0].n_classes})'
-    # table[4,0] = f'CMSC\\textsuperscript{{b}} ({d[1].n_examples}, {d[1].n_classes})'
-    # table[5,0] = f'CBS\\textsuperscript{{c}} ({d[2].n_examples}, {d[2].n_classes})'
-    # table[6,0] = f'DRD\\textsuperscript{{d}} ({d[3].n_examples}, {d[3].n_classes})'
-    # table[18,0] = f'WFR24\\textsuperscript{{e}} ({d[4].n_examples}, {d[4].n_classes})'
+    table[n_d+4,0] = 'Mean pruning time (s)'
+    for i, (name, time) in enumerate(times.items()):
+        table[n_d+4,i+1] = np.mean(time)
+    table[n_d+4,1] = 'N/A'
 
-    # table += """\n\\footnotesize \\textsuperscript{a}Breast Cancer Wisconsin Diagnostic, \\textsuperscript{b}Climate Model Simulation Crashes, \\textsuperscript{c}Connectionist Bench Sonar,\n\n\\textsuperscript{d}Diabetic Retinopathy Debrecen, \\textsuperscript{e}Wall Following Robot 24"""
+    table[n_d+5,0] = 'Mean number of leaves'
+    for i, (name, leave) in enumerate(leaves.items()):
+        table[n_d+5,i+1] = np.mean(leave)
 
     doc.add_package('natbib')
 
 
-    doc.build(delete_files='all', show_pdf=False)
-    # try:
-    #     doc.build(delete_files='all')
-    # except:
-    #     pass
+    print(doc.build(delete_files='all', show_pdf=False))
 
 
 if __name__ == "__main__":
-    process_results(exp_name='exp06-bst18.5')
+    process_results(exp_name='exp06-bst10.5')
     # process_results(exp_name='exp06-less-test')
--- experiments/scripts/process_results_by_dataset.py
+++ experiments/scripts/process_results_by_dataset.py
@@ -1,141 +1,119 @@
+from ctypes import alignment
 import python2latex as p2l
 import sys, os
+
+from xarray import align
 sys.path.append(os.getcwd())
 from datetime import datetime
 import csv
 import numpy as np
+import pandas as pd
+from scipy.stats import t as students_t
 
 from experiments.datasets.datasets import dataset_list
 
 from partitioning_machines import func_to_cmd
 
-
-class MeanWithStd(float, p2l.TexObject):
-    def __new__(cls, array):
-        mean = array.mean()
+class MeanWithCI(float, p2l.TexObject):
+    def __new__(cls, array, confidence_level=.6827):
+        mean = np.mean(array)
         instance = super().__new__(cls, mean)
         instance.mean = mean
-        instance.std = array.std()
+        instance.std = array.std(ddof=1)
+        instance.ci = MeanWithCI.confidence_interval(instance, confidence_level, len(array))
         return instance
 
+    def confidence_interval(self, confidence_level, n_samples):
+        return self.std * students_t.ppf((1+confidence_level)/2, n_samples-1)
+
     def __format__(self, format_spec):
-        if np.isnan(self.mean):
-            return 'N/A'
-        return f'${format(self.mean, format_spec)} \pm {format(self.std, format_spec)}$'
+        return f'${format(self.mean, format_spec)} ({format(self.std, format_spec)})$'
+        # return f'${format(self.mean, format_spec)} \pm {format(self.ci, format_spec)}$'
+        # return f'${format(self.mean, format_spec)}$'
 
 
 def build_table(dataset, exp_name):
-    dataset = dataset.load()
     dataset_name = dataset.name
-    model_names = [
-        'original',
-        'cart',
-        'm-cart',
-        'ours',
-        ]
-    
+    models = {
+        'no_pruning': 'OG',
+        'cart_pruning': 'CC',
+        'reduced_error_pruning': 'RE',
+        'kearns_mansour_pruning': 'KM',
+        'ours_shawe_taylor_pruning': 'Ours',
+        'oracle_pruning': 'Oracle',
+    }
+
+    stats = {
+        'train_accuracy': 'Train acc.',
+        'val_accuracy': 'Val. acc.',
+        'test_accuracy': 'Test acc.',
+        'n_leaves': 'Leaves',
+        'height': 'Height',
+        'time': 'Time $[s]$',
+        'bound': 'Bound'
+    }
+
+    significance = 0.1/100
+
     if isinstance(exp_name, str):
-        exp_names = [exp_name]*4
+        exp_names = [exp_name]*len(models)
     else:
         exp_names = exp_name
 
-    table = p2l.Table((7, 5), float_format='.3f')
+    alignment = r'@{\hspace{6pt}}'.join('l'+'c'*len(models))
+    table = p2l.Table((len(stats)+1, len(models)+1),
+                      float_format='.3f',
+                      alignment=alignment)
     table.body.insert(0, '\\small')
-    table[0,1:] = ['Original', 'CART', 'M-CART', 'Ours']
+
+    table[0,1:] = list(models.values())
     table[0,1:].add_rule()
-    table[1:,0] = ['Train acc.', 'Test acc.', 'Leaves', 'Height', 'Time $[s]$', 'Bound']
+    table[1:,0] = list(stats.values())
 
-    for i, (model, exp) in enumerate(zip(model_names, exp_names)):
-        tr_acc = []
-        ts_acc = []
-        leaves = []
-        height = []
-        bound = []
-        time = []
-        path = './experiments/results/' + dataset_name + '/' + exp + '/' + model + '.csv'
-        try:
-            with open(path, 'r', newline='') as file:
-                reader = csv.reader(file)
-                header = next(reader)
-                for row in reader:
-                    tr_acc.append(row[2])
-                    ts_acc.append(row[3])
-                    leaves.append(row[4])
-                    height.append(row[5])
-                    try:
-                        time.append(row[7])
-                    except:
-                        time.append(np.nan)
-                    if i == 3:
-                        bound.append(row[6])
-                    else:
-                        bound.append(np.nan)
+    for i, (model, exp) in enumerate(zip(models, exp_names)):
+        path = f'./experiments/results/{exp}/{dataset_name}/{model}.csv'
+        with open(path, 'r', newline='') as file:
+            df = pd.read_csv(file, header=0)
 
-            tr_acc = np.array(tr_acc, dtype=float)
-            ts_acc = np.array(ts_acc, dtype=float)
-            leaves = np.array(leaves, dtype=float)
-            height = np.array(height, dtype=float)
-            time = np.array(time, dtype=float)
-            bound = np.array(bound, dtype=float)
-
-            table[0+1, i+1] = MeanWithStd(tr_acc)
-            table[1+1, i+1] = MeanWithStd(ts_acc)
-            table[2+1, i+1] = MeanWithStd(leaves)
-            table[3+1, i+1] = MeanWithStd(height)
-            table[4+1, i+1] = MeanWithStd(time)
-            table[5+1, i+1] = MeanWithStd(bound)
-        except FileNotFoundError:
-            print(f"Could not find file '{path}'")
+        for j, stat in enumerate(stats):
+            if df[stat].isna().any():
+                table[j+1,i+1] = 'NA'
+            else:
+                table[j+1, i+1] = MeanWithCI(df[stat])
 
     dataset_name = dataset_name.replace('_', ' ').title()
+
     dataset_citation = '' if dataset.bibtex_label is None else f'\\citep{{{dataset.bibtex_label}}}'
-    table.caption = f'{dataset_name} Dataset {dataset_citation} ({dataset.n_examples} examples, {dataset.n_features} features, {dataset.n_classes} classes)'
-    table[2,1:].highlight_best(highlight=lambda content: '$\\mathbf{' + content[1:-1] + '}$', atol=.0025, rtol=0)
-    table[3:,1:].change_format('.1f')
+
+    table.caption = f'Statistics for the {dataset_name} data set {dataset_citation}. There are {dataset.n_examples} examples, {dataset.n_features} features ({dataset.n_real_valued_features} real-valued, {len(dataset.ordinal_features)} ordinal and {len(dataset.nominal_features)} nominal), and {dataset.n_classes} classes.'
+
+    table[3,1:-1].highlight_best(best=lambda content: '$\\mathbf{' + content[1:-1] + '}$', atol=significance, rtol=0)
+    table[4:6,1:].format_spec = '.1f'
 
     return table
 
 
 @func_to_cmd
-def process_results(exp_name='first_exp'):
+def process_results(exp_name='exp06-bst10.5'):
     """
     Produces Tables 2 to 20 from the paper (Appendix E). Will try to call pdflatex if installed.
-    
+
     Args:
         exp_name (str): Name of the experiment used when the experiments were run. If no experiments by that name are found, entries are set to 'nan'.
-    
+
     Prints in the console some compiled statistics used in the paper and the tex string used to produce the tables, and will compile it if possible.
     """
-    
-    doc = p2l.Document(exp_name + '_results_by_dataset', '.')
+
+    path = './experiments/results/' + exp_name
+    doc = p2l.Document(exp_name + '_results_by_dataset', path)
     doc.add_package('natbib')
 
     tables = [build_table(dataset, exp_name) for dataset in dataset_list]
-    
-    # Other stats
-    print('Some compiled statistics used in the paper:\n')
-    
-    times_leaves_cart = [table[3,4].data[0][0] / table[3,2].data[0][0] for table in tables]
-    print('CART tree has', sum(times_leaves_cart)/len(times_leaves_cart), 'times less leaves than our model.')
-    acc_gain_vs_cart = [table[2,4].data[0][0] - table[2,2].data[0][0] for table in tables]
-    print('Our model has a mean accuracy gain of', sum(acc_gain_vs_cart)/len(acc_gain_vs_cart), 'over the CART algorithm.')
-    time_ours_vs_cart = [table[5,2].data[0][0] / table[5,4].data[0][0] for table in tables]
-    print('It took in average', sum(time_ours_vs_cart)/len(time_ours_vs_cart), 'less time to prune the tree with our model than with the CART algorithm.')
-
-    times_leaves_mcart = [table[3,3].data[0][0] / table[3,2].data[0][0] for table in tables]
-    print('CART tree has', sum(times_leaves_mcart)/len(times_leaves_mcart), 'times less leaves than the modified CART algorithm.')
-    acc_gain_vs_mcart = [table[2,3].data[0][0] - table[2,2].data[0][0] for table in tables]
-    print('The modified CART algorithm has a mean accuracy gain of', sum(acc_gain_vs_mcart)/len(acc_gain_vs_mcart), 'over the CART algorithm.')
-
-    print('\n')
 
     doc.body.extend(tables)
-    print(doc.build(save_to_disk=False))
-    
-    try:
-        doc.build()
-    except:
-        pass
+    # print(doc.build(save_to_disk=False))
+
+    doc.build(show_pdf=False)
 
 
 if __name__ == "__main__":
